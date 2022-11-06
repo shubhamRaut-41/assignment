c895b92b06f180a4b5f1b5040be4b55a9c483609
shubham raut <shubhamraut666@gmail.com>
2022-11-06 13:10:50 +0530

    updating procfile data gunicorn server

--- userauth/userauth/settings.py
+++ userauth/userauth/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import django_heroku
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -161,3 +163,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
